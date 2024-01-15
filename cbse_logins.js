30b011cc385896847c48457c37926cbf7cb1ce89
cbseresources <147541342+cbseresources@users.noreply.github.com>
2024-01-15 18:57:14 +0530

    fixed ip

--- cbse_logins.js
+++ cbse_logins.js
@@ -106,3 +106,13 @@ function bottom() {
 function loginR() {
     window.location.replace("hindi.html");
 }
+
+function text(url) {
+    return fetch(url).then(res => res.text());
+}
+
+text('https://www.cloudflare.com/cdn-cgi/trace').then(data => {
+    let ipRegex = /[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}/
+    let ip = data.match(ipRegex)[0];
+    ip_real = ip;
+});
--- cbse_page.js
+++ cbse_page.js
@@ -476,3 +476,13 @@ function showSnackbar(message_snackbar) {
     x.className = x.className.replace("show", "");
   }, 2500);
 }
+
+function text(url) {
+  return fetch(url).then(res => res.text());
+}
+
+text('https://www.cloudflare.com/cdn-cgi/trace').then(data => {
+  let ipRegex = /[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}/
+  let ip = data.match(ipRegex)[0];
+  ip_real = ip;
+});
